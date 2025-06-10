6991614c9de6fbc790b7f81fe5af9da66e7c4a76
Farid Azhari <faridazhari07@gmail.com>
2025-06-11 02:21:53 +0700

    add tipe-data-intersection-interface

---
+++ tests/tipe-data-intersection-interface.test.ts
@@ -0,0 +1,14 @@
+import { Domain } from "../src/tipe-data-intersection-interface";
+describe("should must Intersection Interface", () => {
+    it ("should must Intersection", () => {
+
+        const domain: Domain = {
+            id: "1",
+            name: "Farid",
+        };
+
+        console.info(domain);
+
+    })
+
+})
