01d07dae3a6936be5320f31f16a059ae9fec5763
Farid Azhari <faridazhari07@gmail.com>
2025-06-11 02:05:33 +0700

    add tipe-data-function-in-interface

--- src/tipe-data-function-interface.ts
+++ src/tipe-data-function-interface.ts
@@ -1,3 +1,8 @@
 export interface AddFunction {
     (value1: number, value2: number): number;
 }
+
+export interface Person {
+    name: string;
+    sayHello(name: string): string;
+}
--- tests/tipe-data-function-interface.test.ts
+++ tests/tipe-data-function-interface.test.ts
@@ -1,4 +1,5 @@
 import {AddFunction} from "../src/tipe-data-function-interface";
+import {Person} from "../src/tipe-data-function-interface";
 
 describe("Function Interface", () => {
     it ("should must Function Interface", () => {
@@ -9,5 +10,16 @@ describe("Function Interface", () => {
 
         console.info(add(1, 2));
     })
+    it("should must Function in Interface", () => {
+
+        const person: Person = {
+            name: "Farid",
+            sayHello:function (name: string): string {
+                return `Hello ${name}, my name is ${this.name}`;
+            }
+        }
+        console.info(person.sayHello("asep"));
+    })
+
 
 })
