3aff8d6d81559475ce4e7758ac851b4513fb06e6
Farid Azhari <faridazhari07@gmail.com>
2025-06-15 23:51:31 +0700

    add class-property.test.ts

--- tests/class-property.test.ts
+++ tests/class-property.test.ts
@@ -1,25 +1,28 @@
 describe ('class property', () => {
     it ('should must class property', () => {
         class Customer {
+            readonly id: number;
             firstName: string;
             lastName: string;
             balance: number = 0;
+            age?: number;
 
-            constructor(firstName: string = "", lastName: string = "") {
+            constructor(id: number, firstName: string = "", lastName: string = "") {
+                this.id = id;
                 this.firstName = firstName;
                 this.lastName = lastName;
             }
 
             sayHello(): void {
-                console.log(`Hello ${this.firstName} ${this.lastName} ${this.balance}`);
+                console.log(`Hello ${this.id} ${this.firstName} ${this.lastName} ${this.balance}`);
             }
         }
 
         // Contoh penggunaan:
-        const customer = new Customer(); // Tanpa argumen tetap valid karena ada default ""
+        const customer = new Customer(1); // Tanpa argumen tetap valid karena ada default ""
         customer.sayHello();
 
-        const customer2 = new Customer("farid","azhari");
+        const customer2 = new Customer(2,"azhari","nurcahyo");
         customer2.sayHello();
     })
 })
