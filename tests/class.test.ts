04d6d28c74134d717350360afc0971e0d5ddc66c
Farid Azhari <faridazhari07@gmail.com>
2025-06-15 23:52:17 +0700

    add class.test.ts

---
+++ tests/class.test.ts
@@ -0,0 +1,12 @@
+describe('class', () => {
+    it('should must class', () => {
+        class Customer {
+        }
+
+        class Order {
+        }
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+    })
+})
